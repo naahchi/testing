3dde95daa0b7b03637d4a491491c16fdb008778a
naahchi <botpramote@gmail.com>
2026-01-28 20:57:00 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("themeBtn");
+const themeLink = document.getElementById("theme-style");
+
+function setButtonText(theme) {
+  btn.textContent = theme === "dark" ? "Dark" : "Light";
+}
+
+// Page load par (default dark)
+let currentTheme = localStorage.getItem("theme") || "dark";
+setButtonText(currentTheme);
+
+// Toggle
+btn.addEventListener("click", () => {
+  currentTheme = currentTheme === "dark" ? "light" : "dark";
+  themeLink.href = currentTheme === "dark" ? "/css/dark.css" : "/css/light.css";
+  localStorage.setItem("theme", currentTheme);
+  setButtonText(currentTheme);
+});
