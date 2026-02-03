75b73f7c18c785630526394247825e735335380d
naahchi <botpramote@gmail.com>
2026-02-03 16:32:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@ const themeLink = document.getElementById("theme-style");
 
 function setButtonText(theme) {
   // btn.textContent = theme === "dark" ? "Dark" : "Light";
-  btn.innerHTML = theme === "dark" ? "&lt;Dark/&gt;" : "&lt;Light/&gt;";
+  btn.textContent = theme === "light" ? "Light" : "Dark";
 }
 
 // Page load par (default dark)
-let currentTheme = localStorage.getItem("theme") || "dark";
+// let currentTheme = localStorage.getItem("theme") || "dark";
+let currentTheme = localStorage.getItem("theme") || "light";
 setButtonText(currentTheme);
 
 // Toggle
 btn.addEventListener("click", () => {
-  currentTheme = currentTheme === "dark" ? "light" : "dark";
-  themeLink.href = currentTheme === "dark" ? "/css/dark.css" : "/css/light.css";
+  // currentTheme = currentTheme === "dark" ? "light" : "dark";
+  currentTheme = currentTheme === "light" ? "dark" : "light";
+  // themeLink.href = currentTheme === "dark" ? "dark.css" : "light.css";
+  themeLink.href = currentTheme === "light" ? "css/light.css" : "css/dark.css";
   localStorage.setItem("theme", currentTheme);
   setButtonText(currentTheme);
 });
