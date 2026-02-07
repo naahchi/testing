b3741305541bfc44825493a4c738c43815b5d72e
naahchi <botpramote@gmail.com>
2026-02-07 19:35:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ import {
 // 🔹 Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCitBwXw_HF5_tYPIwyyX8sXAs7sH-Pt0A",
-  authDomain: "codemoney-80939.firebaseapp.com",
+  authDomain: "naahchi.github.io",
+  // authDomain: "codemoney-80939.firebaseapp.com",
   projectId: "codemoney-80939",
   appId: "1:243691822378:web:5ddf9db3945714d032d253"
 };
